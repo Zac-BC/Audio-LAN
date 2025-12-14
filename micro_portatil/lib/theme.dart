0d3485a5b0c1e5328fb1216d8916a0bd821a3bc0
Zac-BC <wbarraganacw@gmail.com>
2025-12-14 13:31:02 -0600

    update the structur files to app and choice to theme dark

---
+++ micro_portatil/lib/theme.dart
@@ -0,0 +1,10 @@
+// Esto le da el tema a la app
+
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  scaffoldBackgroundColor: const Color(0xFF0E1420),
+  primaryColor: const Color(0xFF1E5BFF),
+  fontFamily: 'Inter',
+);
